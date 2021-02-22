b47cbc08d66fdf5a977cf20531dae5039d3858fc
Peter  Robinson <drinkcoffee@eml.cc>
2021-02-22 14:40:35 +1000

    Revise method of maintaining list of gateways
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- internal/control/gateway-manager.go
+++ internal/control/gateway-manager.go
@@ -18,11 +18,13 @@ package control
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cid"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/cidoffer"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/fcrcrypto"
 	"github.com/ConsenSys/fc-retrieval-gateway/pkg/logging"
+	"github.com/ConsenSys/fc-retrieval-gateway/pkg/nodeid"
 	"github.com/ConsenSys/fc-retrieval-register/pkg/register"
 
 	"github.com/ConsenSys/fc-retrieval-client/internal/gatewayapi"
@@ -34,70 +36,106 @@ type GatewayManager struct {
 	settings     settings.ClientSettings
 	gateways     []ActiveGateway
 	gatewaysLock sync.RWMutex
+
+	// List of gateway to use. A client may request a node be added to this list e
+	gatewaysToUse []*nodeid.NodeID
+
+	done 				 chan bool
+	ticker       *time.Ticker
 }
 
 // ActiveGateway contains information for a single gateway
 type ActiveGateway struct {
 	info  register.GatewayRegister
 	comms *gatewayapi.Comms
+	nodeID *nodeid.NodeID
 }
 
 // NewGatewayManager returns an initialised instance of the gateway manager.
 func NewGatewayManager(settings settings.ClientSettings) *GatewayManager {
 	g := GatewayManager{}
 	g.settings = settings
+	g.gatewaysToUse = make([]*nodeid.NodeID, 0)
 	g.gatewayManagerRunner()
 	return &g
 }
 
-// TODO this should be in a go routine and loop for ever.
+
+// Get the latest gateway information now.
+func (g *GatewayManager) requestUpdate() {
+
+}
+
+
+
+// gatewayManagerRunner gets the latest gateway information. 
 func (g *GatewayManager) gatewayManagerRunner() {
 	logging.Info("Gateway Manager: Management thread started")
 
-	// Call this once each hour or maybe day.
-	gws, err := register.GetRegisteredGateways(g.settings.RegisterURL())
-	if err != nil {
-		logging.Error("Unable to get registered gateways: %v", err)
-	}
-	logging.Info("Register returned %d gateways", len(gws))
+	g.ticker = time.NewTicker(30 * time.Second)
+	g.done = make(chan bool)
 
-	// TODO this loop is where the managing of gateways that the client is using happens.
-	logging.Info("Gateway Manager: GetGateways returned %d gateways", len(gws))
-	for _, gw := range gws {
-		gatewayID := gw.NodeID
-		if g.gatewayInfoInvalid(&gw) {
-			logging.Warn("Gateway registration information for gateway (%s) is invalid. Ignoring.", gatewayID)
-			continue
+	go func() {
+		for {
+				select {
+				case <-g.done:
+						return
+				case <-g.ticker.C:
+						g.getLatestGatewayInfo()
+				}
 		}
+	}()
+}
 
-		logging.Info("Setting-up comms with: %+v", gw)
-		comms, err := gatewayapi.NewGatewayAPIComms(&gw, &g.settings)
-		if err != nil {
-			panic(err)
+// get the latest gateway information from the registry.
+// Note that this is run inside a go routine. No e
+func (g *GatewayManager) getLatestGatewayInfo() {
+	// Remove any gateways that are no longer in the list of gateways to use.
+	gatewaysToRemove := make([]*nodeid.NodeID, 0)
+	for _, gwInfo := range g.gateways {
+		notFound := true
+		for _, gwNodeID := range g.gatewaysToUse {
+			if gwNodeID.ToString() == gwInfo.nodeID.ToString() {
+				notFound = false
+				break;
+			}
 		}
-
-		// Try to do the establishment with the new gateway
-		var challenge [32]byte
-		fcrcrypto.GeneratePublicRandomBytes(challenge[:])
-		comms.GatewayClientEstablishment(challenge)
-
-		activeGateway := ActiveGateway{gw, comms}
-		g.gateways = append(g.gateways, activeGateway)
+		if notFound {
+			gatewaysToRemove = append(gatewaysToRemove, gwInfo.nodeID)
+		}
+	}
+	for _, gwNodeID := range gatewaysToRemove {
+		for i, gwInfo := range g.gateways {
+			if gwNodeID.ToString() == gwInfo.nodeID.ToString() {
+				g.gateways[i] = g.gateways[len(g.gateways)-1]
+				g.gateways = g.gateways[:len(g.gateways)-1]
+			}
+		}
 	}
 
-	logging.Info("Gateway Manager using %d gateways", len(g.gateways))
+
+	// Get the latest information from the register for exixting gateways
+	// for _, gwInfo := range g.gateways {
+	// 	// TODO
+	// }
+
+
+	// Add information for new gateways.
+	for _, gwNodeID := range g.gatewaysToUse {
+		found := false
+		for _, gwInfo := range g.gateways {
+			if gwNodeID.ToString() == gwInfo.nodeID.ToString() {
+				found = true
+				break;
+			}
+		}
+		if !found {
+			g.addGateway(gwNodeID)
+		}
+	}
 }
 
-// BlockGateway adds a host to disallowed list of gateways
-func (g *GatewayManager) BlockGateway(hostName string) {
-	// TODO
-}
 
-// UnblockGateway add a host to allowed list of gateways
-func (g *GatewayManager) UnblockGateway(hostName string) {
-	// TODO
-
-}
 
 // FindOffersStandardDiscovery finds offers using the standard discovery mechanism.
 func (g *GatewayManager) FindOffersStandardDiscovery(contentID *cid.ContentID) ([]cidoffer.CidGroupOffer, error) {
@@ -132,48 +170,58 @@ func (g *GatewayManager) GetConnectedGateways() []string {
 // Shutdown stops go routines and closes sockets. This should be called as part
 // of the graceful library shutdown
 func (g *GatewayManager) Shutdown() {
-	// TODO
+	g.ticker.Stop()
+	g.done <- true
 }
 
+func (g *GatewayManager) addGateway(nodeID *nodeid.NodeID) {
+	// TODO add gateway by ID
+	gws, err := register.GetRegisteredGateways(g.settings.RegisterURL())
+	if err != nil {
+		logging.Error("Unable to get registered gateways: %v", err)
+		return
+	}
+	logging.Info("Register returned %d gateways", len(gws))
+	if len(gws) == 0 {
+		logging.Warn("Unable to get registered gateways: %v", err)
+		return
+	}
+	if len(gws) != 1 {
+		logging.Warn("Unexpectedly, multiple gateways returned: %d", len(gws))
+		return
+	}
+	gw := gws[0]
+	gatewayID, err := nodeid.NewNodeIDFromString(gw.NodeID)
 
-// Validate the information coming from the Gateway.
-func (g *GatewayManager) gatewayInfoInvalid(gateway *register.GatewayRegister) bool {
-	// All of the fields must have a value in them.
-	if gateway.NodeID == "" {
-		logging.Warn("Gateway registration issue: NodeID not set")
-		return true;
+	if !g.validateGatewayInfo(&gw) {
+		logging.Warn("Gateway registration information for gateway (%s) is invalid. Ignoring.", gatewayID)
+		return
 	}
-	if gateway.Address == "" {
-		logging.Warn("Gateway registration issue: Gateway IP address or domain name not set")
-		return true;
+
+	logging.Info("Setting-up comms with: %+v", gw)
+	comms, err := gatewayapi.NewGatewayAPIComms(&gw, &g.settings)
+	if err != nil {
+		logging.Error("Error encountered which contacting gateway (%s): %+v", gatewayID, err)
+		return
 	}
-	if gateway.NetworkGatewayInfo == "" {
-		logging.Warn("Gateway registration issue: Port for Gateway to Gateway communications not set")
-		return true;
+
+	// TODO this should only be done for new gateways.
+	// Try to do the establishment with the new gateway
+	var challenge [32]byte
+	fcrcrypto.GeneratePublicRandomBytes(challenge[:])
+	comms.GatewayClientEstablishment(challenge)
+	if err != nil {
+		logging.Warn("Error processing node id: %+v", err)
+		return
 	}
-	if gateway.NetworkProviderInfo == "" {
-		logging.Warn("Gateway registration issue: Port for Provider to Gateway communications not set")
-		return true;
+
+	activeGateway := ActiveGateway{gw, comms, gatewayID}
+	g.gateways = append(g.gateways, activeGateway)
+
+	if len(g.gateways) > 0 {
+		logging.Info("Gateway Manager using %d gateways", len(g.gateways))
+	} else {
+		logging.Warn("No gateways available")
 	}
-	if gateway.NetworkClientInfo == "" {
-		logging.Warn("Gateway registration issue: Port for Client to Gateway communications not set")
-		return true;
-	}
-	if gateway.NetworkAdminInfo == "" {
-		logging.Warn("Gateway registration issue: Port for Admin to Gateway communications not set")
-		return true;
-	}
-	if gateway.RegionCode == "" {
-		logging.Warn("Gateway registration issue: Region Code not set")
-		return true;
-	}
-	if gateway.RootSigningKey == "" {
-		logging.Warn("Gateway registration issue: Root Signing Public Key not set")
-		return true;
-	}
-	if gateway.SigingKey == ""	{
-		logging.Warn("Gateway registration issue: Retrieval Signing Key not set")
-		return true;
-	}
-	return false;
 }
+
